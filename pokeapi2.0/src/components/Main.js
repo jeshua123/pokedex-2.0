f505a74bdaffddd557f7831518189f22901ee150
jeshua123 <inv.estevez@gmail.com>
2022-12-11 00:47:25 -0400

    second commit

--- pokeapi2.0/src/components/Main.js
+++ pokeapi2.0/src/components/Main.js
@@ -19,14 +19,6 @@ const Main = () => {
       <div class="card">TWO</div>
       <div class="card">THREE</div>
       <div class="card">FOUR</div>
-      <div class="card">FIVE</div>
-      <div class="card">SIX</div>
-      <div class="card">SEVEN</div>
-      <div class="card">EIGHT</div>
-      <div class="card">NINE</div>
-      <div class="card">TEN</div>
-      <div class="card">ELEVEN</div>
-      <div class="card">TWELVE</div>
     </div>
     
   );
