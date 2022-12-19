269a3d8576fcb1dbaa659340266c5f92ef914626
jeshua123 <inv.estevez@gmail.com>
2022-12-19 02:38:55 -0400

    commit 18-12-2022

--- pokeapi2.0/src/components/Main.js
+++ pokeapi2.0/src/components/Main.js
@@ -6,32 +6,32 @@ import Modal from './Modal';
 const Main = () => {
 
   const [showmodal, setshowmodal] = useState(false)
-  
+  console.log(showmodal);
   return (
     <div className="cards" >
 
-      <div onClick={() => { setshowmodal(true) }}  className="card"  >
+      <div onClick={() => { setshowmodal(true) }} className="card"  >
         <div className="card-info">
           <p className="poke-number"> 123</p>
           <p className="poke-name">nombre Pokemon</p>
           <button type='button' className="button-poke-categorie">water</button>
           <button type='button' className="button-poke-categorie">fire</button>
-        
+
         </div>
         <div className="card-image"><img className="card-img" src={imagen} alt="" /></div>
 
       </div>
-        {showmodal && <Modal setshowmodal={setshowmodal} />}
-      
+      {showmodal && <Modal setshowmodal={setshowmodal} />}
+
       <div className="card"></div>
       <div className="card"></div>
       <div className="card"></div>
       <div className="modal" ></div>
 
     </div>
-   
+
   );
-  
+
 }
 
 export default Main;
