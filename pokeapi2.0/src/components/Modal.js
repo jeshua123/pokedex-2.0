5d3fc5736d2231862c4251bc9c472534b7e695bf
jeshua123 <inv.estevez@gmail.com>
2022-12-19 07:38:26 -0400

    commit navbar 19-12-2022

--- pokeapi2.0/src/components/Modal.js
+++ pokeapi2.0/src/components/Modal.js
@@ -18,8 +18,17 @@ function Modal({ setshowmodal }) {
           <div className="mod-section-3">
             <img src="https://api.lorem.space/image/face?w=150&amp;amp;amp;amp;h=220" alt="" />
           </div>
-          <div className="mod-section-4"></div>
-          <div className="mod-section-5"></div>
+          <div className="mod-section-4">
+            <nav>
+              <a href="1">About</a>
+              <a href="2">Base Stats</a>
+              <a href="3">Evolution</a>
+              <div class="animation start-home"></div>
+            </nav>
+          </div>
+          <div className="mod-section-5">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci accusantium a recusandae ipsam laborum?</p>
+          </div>
           <div className="mod-section-6"></div>
 
         </div>
