909301873584d85f9b716be2d0264d978e2e3328
jeshua123 <inv.estevez@gmail.com>
2022-12-19 04:59:47 -0400

    commit19-12-2022

--- pokeapi2.0/src/components/Modal.js
+++ pokeapi2.0/src/components/Modal.js
@@ -1,20 +1,31 @@
 function Modal({ setshowmodal }) {
-  
-  
+
+
   const handleClick = () => {
     setshowmodal(false)
   }
 
   return (
-<>   
-      <div onClick={() => {handleClick()}} className="div-mod" >
+    <>
+      <div onClick={() => { handleClick() }} className="div-mod" >
         <div className="mod-content" >
-       <p># 123</p>
-          <p>pokemon name</p>
+          <div className="mod-section-1">
+          </div>
+          <div className="mod-section-2">
+            <p># 123</p>
+            <p>pokemon name</p>
+          </div>
+          <div className="mod-section-3">
+            <img src="https://api.lorem.space/image/face?w=150&amp;amp;amp;amp;h=220" alt="" />
+          </div>
+          <div className="mod-section-4"></div>
+          <div className="mod-section-5"></div>
+          <div className="mod-section-6"></div>
+
         </div>
-  
+
       </div>
-</>
+    </>
   )
 }
 
